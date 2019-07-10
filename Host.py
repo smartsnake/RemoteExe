6f6d44882fc16c3f1fa565b92779847b12c1d0a1
Brian Dearing <smartsnake1@gmail.com>
2019-07-10 08:14:19 -0500

    Added Debug to Host.py
    Added some debug outputs for Host.py

--- Host.py
+++ Host.py
@@ -2,34 +2,51 @@ import os
 import json
 import zipfile
 
-#config = None
+alwaysListen = True
+debug = True
+
 def processConfig(config_file):
 #---------loading project config--------------
     with open(config_file) as f:
+        if(debug):
+            print('Loading config.')
         config = json.load(f)
         cmd = 'rm ' + config_file
+        if(debug):
+            print('Deleting ' + config_file)
         os.system(cmd)
         runProgram(config)
 
 def runProgram(config):
+    if(debug):
+            print('Unzipping ' + config['project_folder'])
     zip_ref = zipfile.ZipFile(config['project_folder'] + '.zip', 'r')
     zip_ref.extractall('..')
     zip_ref.close()
 
+    if(debug):
+            print('Deleting ' + config['project_folder'] + '.zip')
     cmd = 'rm ' + config['project_folder'] + '.zip'
-    print(cmd)
     os.system(cmd)
 
-    #Run Program
-    os.system(config['run'])
-    
-    #print("Running")
+    #Compile Program
+    if(config['compile'] != None):
+        if(debug):
+            print('Compiling program...')
+        os.system(config['compile'])
 
-#while True:
-for file in os.listdir('.'):
-    if file.endswith('.project'):
-        #print(file)
-        processConfig(file)
-        break
-else:
-    print("Not found")
+    #Run Program
+    print("Run Complete: " + config['project_folder'])
+    os.system(config['run'])
+
+
+while alwaysListen:
+    for file in os.listdir('.'):
+        if file.endswith('.project'):
+            #print(file)
+            if(debug):
+                print('Found project: ' + file)
+            processConfig(file)
+            break
+    else:
+        print("Not found")
